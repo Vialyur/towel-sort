482031282840e084e4834f6c20e03e71ccc74eeb
Vital Yurau <vialyur@list.ru>
2020-03-23 21:01:10 +0300

    Added conditions for empty matrix or not parametres

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = function towelSort (matrix) {
   let result = [];
-  matrix.map((el, i) => {result.push((i % 2 !== 0) ? el.reverse() : el)});
-  return result.flat(Infinity)  
+  if (matrix && matrix.length !== 0)
+  {matrix.map((el, i) => {result.push((i % 2 !== 0) ? el.reverse() : el)});
+  return result.flat(Infinity)}
+  else return [];  
 }
